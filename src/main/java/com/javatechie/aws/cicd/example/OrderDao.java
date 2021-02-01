28fdc2fdf9f8ca4211cf2869dc08d7a3452ca5b8
gilardoni72 <52677292+gilardoni72@users.noreply.github.com>
2021-01-31 19:17:38 -0600

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -15,7 +15,7 @@ public class OrderDao {
                 new Order(101, "Gilardoni", 1, 30000),
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
-                new Order(809, "headset", 1, 1799))
+                new Order(102, "Jaraheadset", 1, 1799))
                 .collect(Collectors.toList());
     }
 }
